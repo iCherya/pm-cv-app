437d594d89b2818a360de109ece0a0847f694bb5
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-16 06:46:35 +0200

    Initial project setting

---
+++ src/App/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import styles from './App.module.css';
+import PhotoAlbum from '../components/PhotoAlbum/PhotoAlbum';
+import PhotoItem from '../components/PhotoItem/PhotoItem';
+import NotFound from '../components/NotFound/NotFound';
+
+const App = () => (
+  <BrowserRouter>
+    <div className={styles.wrapper}>
+      <Switch>
+        <Route path="/album/:id">
+          <PhotoAlbum />
+        </Route>
+        <Route path="/photo/:id">
+          <PhotoItem />
+        </Route>
+        <Route exact path="/">
+          <PhotoAlbum />
+        </Route>
+        <Route path="/">
+          <NotFound />
+        </Route>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default App;
